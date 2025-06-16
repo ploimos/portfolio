a25b02a0a2066ccb44ab911fec43acd6f9ef7bb1
Marco Plomitallo <m.plomitallo@outlook.com>
2025-06-16 04:59:24 +0200

    Header modulare

---
+++ assets/js/load-components.js
@@ -0,0 +1,11 @@
+async function loadComponent(path, targetId) {
+  const response = await fetch(path);
+  const html = await response.text();
+  document.getElementById(targetId).innerHTML = html;
+}
+
+// Carica header e footer
+document.addEventListener('DOMContentLoaded', () => {
+  loadComponent('includes/header.html', 'header');
+  //loadComponent('includes/footer.html', 'footer');
+});
